ca1c97a6cce42b864b26a6fc822dd97205035b80
Sourav Padhy <techguysourav@gmail.com>
2024-06-12 16:45:24 +0530

    changes done

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,11 +1,11 @@
-const name = "hitesh"
+const name = "sourav"
 const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('souravpa')
+const gameName = new String('souravpadhy')
 console.log(gameName[0]);
 console.log(gameName.__proto__);
 
